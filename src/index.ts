f57be08ee59d8a439224bad6a36dc63c1993d47e
kgarcesp <kgarcesp@tierragro.co>
2025-08-28 15:20:25 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3003;
 
-app.listen(3003, '0.0.0.0', () => {
+app.listen(3003, () => {
   console.log('Servidor escuchando en http://0.0.0.0:3003');
 });
 
