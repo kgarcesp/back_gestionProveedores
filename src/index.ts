e4c77d587608d1e8c56451167aa2c5df7f2e9107
kgarcesp <kgarcesp@tierragro.co>
2025-08-29 10:47:27 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import app from "./app";
+import https from "https";
+import fs from "fs";
 
 const PORT = process.env.PORT || 3003;
 
-app.listen(3003,  "0.0.0.0", () => {
-  console.log('Servidor escuchando en http://0.0.0.0:3003');
-});
+// Intentamos leer los certificados solo si existen (QA)
+let server: any;
+try {
+  const options = {
+    key: fs.readFileSync("qa.tierragro.com.key"),
+    cert: fs.readFileSync("qa.tierragro.com.crt"),
+    ca: fs.readFileSync("qa.intermediate.crt"),
+  };
 
+  server = https.createServer(options, app);
+  server.listen(PORT, () => {
+    console.log(`Servidor HTTPS escuchando en https://qa.tierragro.com:${PORT}`);
+  });
+} catch (err) {
+  console.warn(" No se encontraron certificados, iniciando en HTTP...");
+  app.listen(PORT, () => {
+    console.log(`Servidor HTTP escuchando en http://localhost:${PORT}`);
+  });
+}
