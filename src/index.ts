c8fac9830f83ac6dead2c2b9a3aad7681238f308
kgarcesp <kgarcesp@tierragro.co>
2025-08-29 13:38:38 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import app from "./app";
+import https from "https";
+import http from "http";
+import fs from "fs";
 
 const PORT = process.env.PORT || 3003;
 
-app.listen(3003,  "0.0.0.0", () => {
-  console.log('Servidor escuchando en http://0.0.0.0:3003');
-});
+// Verificamos si existen certificados para levantar HTTPS
+let server;
+if (fs.existsSync("qa.tierragro.com.key") && fs.existsSync("qa.tierragro.com.crt")) {
+  const options = {
+    key: fs.readFileSync("qa.tierragro.com.key"),
+    cert: fs.readFileSync("qa.tierragro.com.crt"),
+    ca: fs.readFileSync("qa.intermediate.crt"),
+  };
 
+  server = https.createServer(options, app);
+  server.listen(3003, "0.0.0.0", () => {
+    console.log(`✅ Servidor HTTPS escuchando en https://0.0.0.0:${PORT}`);
+  });
+} else {
+  // fallback a HTTP (local/dev)
+  server = http.createServer(app);
+  server.listen(3003, "0.0.0.0", () => {
+    console.log(`✅ Servidor HTTP escuchando en http://0.0.0.0:${PORT}`);
+  });
+}
