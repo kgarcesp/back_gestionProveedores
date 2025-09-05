8b6771cbac016d4b6feed8bb0c83120126f68779
kgarcesp <kgarcesp@tierragro.co>
2025-09-05 08:48:01 -0500

    refactor: config

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,26 @@ import https from "https";
 import http from "http";
 import fs from "fs";
 
-const PORT = process.env.PORT || 3003;
+const PORT = Number(process.env.PORT) || 3003;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
-// Si existen los certificados levantamos HTTPS
-if (fs.existsSync("qa.tierragro.com.key") && fs.existsSync("qa.tierragro.com.crt") && fs.existsSync("qa.intermediate.crt")) {
-    const options = {
-        key: fs.readFileSync("qa.tierragro.com.key"),
-        cert: fs.readFileSync("qa.tierragro.com.crt"),
-        ca: fs.readFileSync("qa.intermediate.crt")
-    };
+if (
+  NODE_ENV === "production" && // solo en producción usar HTTPS
+  fs.existsSync("qa.tierragro.com.key") &&
+  fs.existsSync("qa.tierragro.com.crt") &&
+  fs.existsSync("qa.intermediate.crt")
+) {
+  const options = {
+    key: fs.readFileSync("qa.tierragro.com.key"),
+    cert: fs.readFileSync("qa.tierragro.com.crt"),
+    ca: fs.readFileSync("qa.intermediate.crt"),
+  };
 
-    https.createServer(options, app).listen(3003, "0.0.0.0", () => {
-        console.log(`✅ Servidor HTTPS escuchando en https://0.0.0.0:${PORT}`);
-    });
+  https.createServer(options, app).listen(PORT, "0.0.0.0", () => {
+    console.log(`✅ Servidor HTTPS escuchando en https://0.0.0.0:${PORT}`);
+  });
 } else {
-    // Fallback local/dev
-    http.createServer(app).listen(3003, "0.0.0.0", () => {
-        console.log(`⚠️ Servidor HTTP escuchando en http://0.0.0.0:${PORT}`);
-    });
+  http.createServer(app).listen(PORT, "0.0.0.0", () => {
+    console.log(`⚠️ Servidor HTTP escuchando en http://0.0.0.0:${PORT}`);
+  });
 }
-
